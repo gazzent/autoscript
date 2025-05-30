2c2aafef0ed230599fb21ce55509e8bad8d853aa
gazzent <158091173+gazzent@users.noreply.github.com>
2025-05-30 22:10:52 +0700

    Update m-ip.sh

--- m-ip.sh
+++ m-ip.sh
@@ -5,7 +5,7 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ipsaya=$(wget -qO- ipinfo.io/ip)
 data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 date_list=$(date +"%Y-%m-%d" -d "$data_server")
-data_ip="https://raw.githubusercontent.com/AngIMAN/permission/main/seler"
+data_ip="https://raw.githubusercontent.com/gazzent/reg/main/ip"
 checking_sc() {
   useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
   if [[ $date_list < $useexp ]]; then
@@ -18,8 +18,8 @@ checking_sc() {
     echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas Gak Di izinin${NC}"
     echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
     echo -e "             \033[0;33mContact Admin :${NC}"
-    echo -e "         \033[0;36mTelegram${NC} t.me/AimanVpnExpress"
-    echo -e "      ${GREEN}WhatsApp${NC} wa.me/+628981874211"
+    echo -e "         \033[0;36mTelegram${NC} t.me//Candravpnz"
+    echo -e "      ${GREEN}WhatsApp${NC} wa.me/+6281573872702"
     echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
 read -n 1 -s -r -p "  Press any key to back on menu"
 menu
